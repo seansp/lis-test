128606440634be9e3df30405ca8addf2dd61e346
Sean Spratt <seansp@gmail.com>
2019-03-31 18:51:01 -0700

    ...

--- lsvm/Test1.ps1
+++ lsvm/Test1.ps1
@@ -1,11 +1,18 @@
 Write-Host "Test1"
 Import-Module .\Utilities.psm1
 
+
+#Delete old test.
 CleanUp-VM "Shielded_PRE-TDC"
 
+#Add drive to mount encrypted drive.
 $decryptSource = "D:\WSSCFS\TestContent\Shielded\DecryptVHD\decrypt_drive.vhdx"
 $pathToDecryptionVHD = "..\decyptionDrive.vhdx"
+Copy-Item -Destination $pathToDecryptionVHD -Path $decryptSource -Force
 
-Copy-Item -Path $pathToDecryptionVHD -Source $decryptSource
+#Copy the test drive.
+$testDriveSource = "D:\WSSCFS\VHD\Cloudbase\Shielded_Encrypted_VHDs\ubuntu1604_encrypted.vhdx"
+$pathToTestVHD = "..\testVHD.vhdx"
+Copy-Item -Destination $pathToDecryptionVHD -Path $decryptSource -Force
 
-
+dir ..
