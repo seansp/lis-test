28af3568ddbe4b62c3ce68f51ee5c066ea3d48bd
Sean Spratt <seansp@gmail.com>
2019-03-31 18:09:40 -0700

    ...

--- lsvm/Test1.ps1
+++ lsvm/Test1.ps1
@@ -1,10 +1,5 @@
 Write-Host "Test1"
 Import-Module .\Utilities.psm1
 
-$r1 = FindVM "Shielded_PRE-TDC"
 
-Write-Host "R1 == $r1"
-
-$r2 = FindVM "Foo" 
-
-Write-Host "R2 = $r2"
+CleanUp-VM Shielded_PRE-TDC
--- lsvm/Utilities.psm1
+++ lsvm/Utilities.psm1
@@ -1,5 +1,24 @@
 Write-Host "Utilities Loaded."
 
+
+function Cleanup-VM ([string]$vmName)
+{
+    $vm = Get-VM -Name $vmName
+    if( $vm )
+    {
+        Write-Host "VM $vmName -- exists."
+        $hd = $vm.HardDrives[0].Path
+        Write-Host "Hard Drive Location -- $hd"
+        Write-Host "Stopping VM"
+        Stop-VM $vm -TurnOff -ErrorAction SilentlyContinue
+        Remove-VM $vm -Confirm:$false -Force -ErrorAction SilentlyContinue
+        Remove-Item -Path $hd -Force
+    }
+}
+
+
+
+
 function FindVM( [string] $vmName )
 {
     $vm = Get-VM -Name $vmName
