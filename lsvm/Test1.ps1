54b47b2d2c5e3548fd9d858a54210abaf87aa1b5
Sean Spratt <seansp@gmail.com>
2019-03-31 19:08:31 -0700

    ...

--- lsvm/Test1.ps1
+++ lsvm/Test1.ps1
@@ -2,8 +2,13 @@ Write-Host "Test1"
 Import-Module .\Utilities.psm1
 
 
+$testVMName = "LSVM_PRE-TDC"
+$testSwitchName = 'External'
+$testMemory = 2048MB
+
+
 #Delete old test.
-CleanUp-VM "Shielded_PRE-TDC"
+CleanUp-VM $testVMName
 
 #Add drive to mount encrypted drive.
 $decryptSource = "D:\WSSCFS\TestContent\Shielded\DecryptVHD\decrypt_drive.vhdx"
@@ -15,4 +20,6 @@ $testDriveSource = "D:\WSSCFS\VHD\Cloudbase\Shielded_Encrypted_VHDs\ubuntu1604_e
 $pathToTestVHD = "..\testVHD.vhdx"
 Copy-Item -Destination $pathToTestVHD -Path $testDriveSource -Force
 
-dir ..
+#Create the Test VM
+New-VM -Name $testVMName -Generation 2 -VHDPath $pathToTestVHD -MemoryStartupBytes $testMemory -SwitchName $testSwitchName
+Set-VMFirmware -VMName $testVMName -EnableSecureBoot Off
